c8508d2cc3e42ff10f3bf0ffd97e30efd9631bd5
Emrecsmsk <64790879+Emrecsmsk@users.noreply.github.com>
2022-09-27 10:30:38 +0300

    Delete build directory

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/emrecan/tekstilstok/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/emrecan/tekstilstok/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/Objects-normal/arm64/path_provider_macos-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/emrecan/tekstilstok/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-dummy.m \
-  /Users/emrecan/tekstilstok/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/module.modulemap \
-  /Users/emrecan/tekstilstok/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-prefix.pch
